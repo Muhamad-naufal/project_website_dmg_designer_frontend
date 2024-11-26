4512b7806c1d31b87ee68123fa5844d3ea26e40b
muhamad-naufal <muhamadnaufal594@gmail.com>
2024-11-26 16:01:51 +0700

    done project

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,18 +1,45 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 export default function CallToAction() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <section className="py-24">
-            <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
-                    {Array.from({ length: 10 }).map((_, i) => (
-                        <div key={i} className="flex items-center gap-16">
-                            <span className="text-lime-400 text-7xl">
-                                &#10038;
-                            </span>
-                            <span>Coba Gratis</span>
-                        </div>
-                    ))}
-                </div>
-            </div>
+        <section className="py-24 overflow-hidden">
+            <motion.div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                className="flex whitespace-nowrap"
+                animate={{
+                    x: isHovered ? "0%" : "-100%",
+                }}
+                transition={{
+                    ease: "linear",
+                    duration: 30,
+                    repeat: Infinity,
+                }}
+            >
+                {/* Isi teks berganda untuk ilusi loop */}
+                {Array.from({ length: 2 }).map((_, idx) => (
+                    <div
+                        key={idx}
+                        className="flex flex-none pr-16 gap-16 text-7xl group cursor-pointer md:text-8xl font-medium"
+                    >
+                        {Array.from({ length: 10 }).map((_, i) => (
+                            <div key={i} className="flex items-center gap-16">
+                                <span className="text-lime-400 text-7xl">
+                                    &#10038;
+                                </span>
+                                <span className="group-hover:text-lime-400">
+                                    Coba Gratis
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </motion.div>
         </section>
     );
 }
